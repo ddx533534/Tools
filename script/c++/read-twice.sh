84287f28ba313461cf0a79bf0b8b5fd304527981
dudongxu <dudongxu@meituan.com>
2023-09-28 16:07:35 +0800

    测试页面缓存读,页面调入次数

--- script/c++/read-twice.sh
+++ script/c++/read-twice.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 结合 sar -B 命令可以采集文件内容读入页面缓存时，总共发生多少次页面调入？
+# 结合 sar -B 1 命令可以采集文件内容读入页面缓存时，总共发生多少次页面调入？
 rm -f testfile
 echo "$(date):start file creation!"
 dd if=/dev/zero of=testfile oflag=direct bs=1M count=1K
@@ -11,6 +11,10 @@ echo "$(date):end 3s sleep"
 echo "$(date):start 1st read"
 cat testfile >/dev/null 
 echo "$(date):end 1st read"
+
+echo "$(date):start 3s sleep"
+sleep 3
+echo "$(date):end 3s sleep"
 
 echo "$(date):start 2nd read"
 cat testfile >/dev/null 
