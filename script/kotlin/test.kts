9b585a38501a75896538f609567665bac1ef228e
dudongxu <dudongxu@meituan.com>
2023-12-18 10:12:50 +0800

    test

--- script/kotlin/test.kts
+++ script/kotlin/test.kts
@@ -60,11 +60,115 @@ fun testCollection()  {
 
 try {
     val array = arrayOf(5, 3, 6, 4, 2, 1)
-    insertSortWithBinary(array)
-} catch (e: Exception) {
+    val complex = Complex(1, -1) * Complex(1, -1)
+    // println("ddx".revert())
+        for (i in "a".."ab") {
+        println(i)
+    }
+    } catch (e: Exception) {
     println(e.getStackTrace()[0])
     println(e.getStackTrace()[1])
 }
+
+
+data class Complex(val real: Int, val imaginary: Int) {
+    operator fun plus(complex: Complex): Complex {
+        return Complex(real + complex.real, imaginary + complex.imaginary)
+    }
+
+    operator fun times(complex: Complex): Complex {
+        val r = real * complex.real - (imaginary * imaginary)
+        val i = real * complex.imaginary + imaginary * complex.real
+        return Complex(r, i)
+    }
+
+    override fun toString(): String {
+        return "$real${if (imaginary > 0) "+" else "-"}${kotlin.math.abs(imaginary)}i"
+    }
+    fun pureImaginary(): Boolean {
+        println("I am class instance!")
+        return real == 0
+    }
+
+    companion object {
+        fun pureImaginary(): Boolean {
+            println("I am companion object!")
+            return false
+        }
+    }
+}
+// 扩展函数
+fun Complex.pureImaginary(): Boolean {
+    println("I am class extention!")
+    return real == 0
+}
+// 扩展属性
+var Complex.isReal: Boolean
+    get() = imaginary == 0
+    set(value) {
+        isReal = value
+    }
+
+
+// 注入第三方类
+fun String.revert(): String {
+    val res = StringBuilder()
+    for (i in length - 1 downTo 0) {
+        res.append(get(i))
+    }
+    return res.toString()
+}
+
+// 修复 String 的迭代问题
+operator fun ClosedRange<String>.iterator() = object : Iterator<String> {
+
+    private val next = StringBuilder(start)
+    private val last = endInclusive
+
+    override fun hasNext(): Boolean {
+        println(last >= next.toString())
+        return last >= next.toString() && last.length >= next.length
+    }
+
+    override fun next(): String {
+        val result = next.toString()
+        val lastCharacter = next.last()
+        if (lastCharacter < Char.MAX_VALUE) {
+            next.setCharAt(next.length - 1, lastCharacter + 1)
+        } else {
+            next.append(Char.MAX_VALUE)
+        }
+        return result
+    }
+
+}
+
+
+
+inline fun invoke(n: Int, action: (Int) -> Unit) {
+    action(n)
+}
+
+
+fun test() {
+
+    println("enter test")
+
+    (0..3).forEach {
+        invoke(it) {
+            println("enter invoke lambda")
+            if(it == 2){
+                return
+            }
+            println(it)
+            println("exit invoke lambda")
+        }
+    }
+    
+    println("exit test")
+}
+
+
 
 fun insertSort(array: Array<Int>?) {
     array?.takeIf {
