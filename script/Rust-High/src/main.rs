ba09a14320888bf2e746308a661487641d25db27
dudongxu <dudongxu@meituan.com>
2024-03-25 20:02:53 +0800

    添加迭代器

--- script/Rust-High/src/main.rs
+++ script/Rust-High/src/main.rs
@@ -1,12 +1,17 @@
-use crate::closure::{test, test_closure};
+use crate::iterator::{test_into_iterator, test_iterator, test_iterator_adapter, test_iterator_self};
 use crate::lifecycle::{test_life_cycle1, test_reborrow};
 
 mod lifecycle;
 mod closure;
+mod iterator;
 
 fn main() {
-    test_life_cycle1();
-    test_reborrow();
-    test();
-    test_closure();
+    // test_life_cycle1();
+    // test_reborrow();
+    // test();
+    // test_closure();
+    // test_iterator();
+    // test_into_iterator();
+    // test_iterator_self();
+    test_iterator_adapter();
 }
